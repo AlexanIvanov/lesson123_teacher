82fc70ee592024ee6d0a1f42a9d91ed0d3d3d091
zhassulanbaimukhamedov <48639707+zhassulanbaimukhamedov@users.noreply.github.com>
2022-02-06 15:01:42 +0600

    teacher commit 1

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h4>Hello from Teacher</h4>
+        <h4>Teacher has change some code</h4>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
